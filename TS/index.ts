d6bd9a87484f4569a0e79200a4682fef6a18523e
Sameer Shaikh <sshaikh77096656@gmail.com>
2022-05-27 18:42:05 +0530

    removed hardcoded values

--- TS/index.ts
+++ TS/index.ts
@@ -1,56 +1,67 @@
 import puppeteer from 'puppeteer';
 import * as fs from 'fs';
 
-const buttonsListSelector = '.variant-section .arrow_box';
+// Variables to store and reuse CSS selectors
 const skuSelector = '#AddToCartForm .sku span';
-type dataType = {[key: string]: (string|number|dataType)}
+const variantSectionsSelector = '.variant-section.clearfix';
+const variantClickableSelector = '.arrow_box span';
+const productNameSelector = '.title';
+const priceSelector = '#current-price';
+type dataType = {[key: string]: (string|number|dataType)};
 
 (async () => {
     let url = 'https://teradek.com/collections/colr/products/anton-bauer-digital-battery?variant=14579104677933';
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0); 
+    page.setDefaultNavigationTimeout(0); 
     await page.goto(url);    
 
-    let capacities = ['99Wh','156Wh']
-    let mount = ['Gold-Mount','V-Mount']
-    // There are a total of 4 combinations of capacity and mount options
-    // we simulate these options by clicking the buttons we access through DOM
-    
-    
+    // Calculating the number of variants to different combinations
+    // we simulate these options by clicking the clickables we access through DOM
+    let t: puppeteer.ElementHandle[] = await page.$$(variantSectionsSelector)
+    const capacities: number = await t[0].$$eval(variantClickableSelector,a => a.length)
+    const mount: number = await t[1].$$eval(variantClickableSelector,a => a.length)
     let data: dataType = {};
+
     try {
-        for(let i = 0; i < capacities.length; i++) {
-            
-            // Fetching the buttons that we want to click. This list contains 4 buttons of which first 2 represent Capacity,
-            // And last two represent Mount
-            let buttons: puppeteer.ElementHandle[] = await page.$$(buttonsListSelector);
-            await buttons[i].click()
+        for(let i = 0; i < capacities; i++) {
+            // Accessing the clicable and simulating a click
+            let keyPart1: string = await page.evaluate((i: number,variantSectionsSelector: string,variantClickableSelector: string): string => {
+                let button: HTMLElement = document.querySelectorAll(variantSectionsSelector)[0].querySelectorAll(variantClickableSelector)[i] as HTMLElement;
+                button.click();
+                return button.innerText;
+            },i,variantSectionsSelector,variantClickableSelector)
 
             // Waiting for information to load
             await page.waitForSelector(skuSelector)
-            await page.waitForSelector('#current-price')
+            await page.waitForSelector(priceSelector)
 
-            for(let j = 0; j < mount.length; j++) {
-                buttons = await page.$$(buttonsListSelector);
-                await buttons[j+2].click();
+            for(let j = 0; j < mount; j++) {
+                // Accessing the clicable and simulating a click
+                let keyPart2: string = await page.evaluate((j: number,variantSectionsSelector: string,variantClickableSelector: string): string => {
+                    let button: HTMLElement = document.querySelectorAll(variantSectionsSelector)[1].querySelectorAll(variantClickableSelector)[j] as HTMLElement;
+                    button.click();
+                    return button.innerText;
+                },j,variantSectionsSelector,variantClickableSelector)
 
                 // Waiting for information to load
                 await page.waitForSelector(skuSelector);
-                await page.waitForSelector('#current-price')
+                await page.waitForSelector(priceSelector)
 
-                let values = await page.evaluate((): dataType => {
-                    let productName = (document.querySelector('.title') as HTMLElement).innerText;
-                    let SKU = (document.querySelector('#AddToCartForm .sku span') as HTMLElement).innerText;
-                    let price = (document.getElementById('current-price') as HTMLElement).innerText;
+                let values = await page.evaluate((productNameSelector: string,skuSelector: string,priceSelector: string) => {
+                    
+                    let productName = (document.querySelector(productNameSelector) as HTMLElement).innerText;
+                    let SKU = (document.querySelector(skuSelector) as HTMLElement).innerText;
+                    let price = (document.querySelector(priceSelector) as HTMLElement).innerText;
                     return {
                         "Product Name":productName,
                         "SKU":SKU,
                         "Price":price
                     }
-                });
-                // Generating key for different configurations
-                let key = capacities[i] + ' + ' + mount[j]
+
+                },productNameSelector,skuSelector,priceSelector);
+                // Generating the key
+                let key = keyPart1 + ' + ' + keyPart2
                 data[key] = {...values}
             }
         }
--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const buttonsListSelector = '.variant-section .arrow_box';
+// Variables to store and reuse CSS selectors
 const skuSelector = '#AddToCartForm .sku span';
+const variantSectionsSelector = '.variant-section.clearfix';
+const variantClickableSelector = '.arrow_box span';
+const productNameSelector = '.title';
+const priceSelector = '#current-price';
 
 (async () => {
     let url = 'https://teradek.com/collections/colr/products/anton-bauer-digital-battery?variant=14579104677933';
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0); 
+    page.setDefaultNavigationTimeout(0); 
     await page.goto(url);    
 
-    let capacities = ['99Wh','156Wh']
-    let mount = ['Gold-Mount','V-Mount']
-    // There are a total of 4 combinations of capacity and mount options
-    // we simulate these options by clicking the buttons we access through DOM
+    // Calculating the number of variants to different combinations
+    // we simulate these options by clicking the clickables we access through DOM
+    let t = await page.$$(variantSectionsSelector)
+    const capacities = await t[0].$$eval(variantClickableSelector,a => a.length)
+    const mount = await t[1].$$eval(variantClickableSelector,a => a.length)
     let data = {};
+    
     try {
-        for(let i = 0; i < capacities.length; i++) {
-            // Fetching the buttons that we want to click
-            let buttons = await page.$$(buttonsListSelector);
-            await buttons[i].click()
+        for(let i = 0; i < capacities; i++) {
+            // Accessing the clicable and simulating a click
+            let keyPart1 = await page.evaluate((i,variantSectionsSelector,variantClickableSelector) => {
+                let button = document.querySelectorAll(variantSectionsSelector)[0].querySelectorAll(variantClickableSelector)[i];
+                button.click();
+                return button.innerText;
+            },i,variantSectionsSelector,variantClickableSelector)
 
             // Waiting for information to load
             await page.waitForSelector(skuSelector)
-            await page.waitForSelector('#current-price')
+            await page.waitForSelector(priceSelector)
 
-            for(let j = 0; j < mount.length; j++) {
-                buttons = await page.$$(buttonsListSelector);
-                await buttons[j+2].click();
+            for(let j = 0; j < mount; j++) {
+            // Accessing the clicable and simulating a click
+                let keyPart2 = await page.evaluate((j,variantSectionsSelector,variantClickableSelector) => {
+                    let button = document.querySelectorAll(variantSectionsSelector)[1].querySelectorAll(variantClickableSelector)[j];
+                    button.click();
+                    return button.innerText;
+                },j,variantSectionsSelector,variantClickableSelector)
 
                 // Waiting for information to load
                 await page.waitForSelector(skuSelector);
-                await page.waitForSelector('#current-price')
+                await page.waitForSelector(priceSelector)
 
-                let values = await page.evaluate(() => {
-                    let productName = document.querySelector('.title').innerText;
-                    let SKU = document.querySelector('#AddToCartForm .sku span').innerText;
-                    let price = document.getElementById('current-price').innerText;
+                let values = await page.evaluate((productNameSelector,skuSelector,priceSelector) => {
+                    
+                    let productName = document.querySelector(productNameSelector).innerText;
+                    let SKU = document.querySelector(skuSelector).innerText;
+                    let price = document.querySelector(priceSelector).innerText;
                     return {
                         "Product Name":productName,
                         "SKU":SKU,
                         "Price":price
                     }
-                });
-                // Generating key for different configurations
-                let key = capacities[i] + ' + ' + mount[j]
+
+                },productNameSelector,skuSelector,priceSelector);
+                // Generating the key
+                let key = keyPart1 + ' + ' + keyPart2
                 data[key] = {...values}
             }
         }
